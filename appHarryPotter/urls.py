b6a190240fdd5b606cc9883c1aa2a6b7bf86787b
AnderPalacios <145034768+AnderPalacios@users.noreply.github.com>
2024-11-30 23:02:57 +0100

    Acceder portada sin poner nombre de la app

--- appHarryPotter/urls.py
+++ appHarryPotter/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name='index'),
- path('categorias', views.show_categorias, name="categorias"),
- path('categorias/<int:categoria_id>/criaturas/', views.criaturas_por_categoria, name='criaturas_por_categoria'),
- path('razas', views.show_razas, name="razas"),
- path('razas/<int:raza_id>/criaturas/', views.criaturas_por_raza, name='criaturas_por_raza'),
- path('criaturas', views.show_criaturas, name="criaturas"),
- path('criatura/<int:criatura_id>/', views.ver_criatura, name='criatura'),
+ path('appHarryPotter/categorias', views.show_categorias, name="categorias"),
+ path('appHarryPotter/categorias/<int:categoria_id>/criaturas/', views.criaturas_por_categoria, name='criaturas_por_categoria'),
+ path('appHarryPotter/razas', views.show_razas, name="razas"),
+ path('appHarryPotter/razas/<int:raza_id>/criaturas/', views.criaturas_por_raza, name='criaturas_por_raza'),
+ path('appHarryPotter/criaturas', views.show_criaturas, name="criaturas"),
+ path('appHarryPotter/criatura/<int:criatura_id>/', views.ver_criatura, name='criatura'),
 ]
