09adbfb47644d061aa2983846eec1f95a17ccef9
aratz <ianaratz.bergado@opendeusto.es>
2024-12-02 13:23:01 +0100

    Formularios

--- appHarryPotter/urls.py
+++ appHarryPotter/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.i18n import i18n_patterns
 from . import views
 
 # De esta manera soporta el i18n para el muntilingüismo
-urlpatterns = i18n_patterns(
+urlpatterns = i18n_patterns([
  path('', views.index, name='index'),
  path('appHarryPotter/categorias', views.show_categorias, name="categorias"),
  path('appHarryPotter/categorias/<int:categoria_id>/criaturas/', views.criaturas_por_categoria, name='criaturas_por_categoria'),
@@ -11,4 +11,5 @@ urlpatterns = i18n_patterns(
  path('appHarryPotter/razas/<int:raza_id>/criaturas/', views.criaturas_por_raza, name='criaturas_por_raza'),
  path('appHarryPotter/criaturas', views.show_criaturas, name="criaturas"),
  path('appHarryPotter/criatura/<int:criatura_id>/', views.ver_criatura, name='criatura'),
-]
+path('appHarryPotter/formulario/', views.formulario, name='formulario'),  # Nueva URL para el formulario
+])
