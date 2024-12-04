4279a0bdab59d873dfad216690acf79aa54ccf10
aratz <ianaratz.bergado@opendeusto.es>
2024-12-04 18:58:52 +0100

    botón enviar funciona correctamente

--- appHarryPotter/forms.py
+++ appHarryPotter/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Criatura
+from django.core.exceptions import ValidationError
+
 
 class CriaturaForm(forms.ModelForm):
     class Meta:
         model = Criatura
         fields = ['nombre', 'descripcion', 'tamano', 'raza', 'categorias_peligro']  
         widgets = {
-            'categorias_peligro': forms.RadioSelect,
+            'categorias_peligro': forms.CheckboxSelectMultiple,
             'raza': forms.Select(),
         }
