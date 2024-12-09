922247b295e7a1f4011078777f85836b7621a3d2
AnderPalacios <145034768+AnderPalacios@users.noreply.github.com>
2024-12-09 13:20:49 +0100

    últimas trads

--- IW_BI_14/urls.py
+++ IW_BI_14/urls.py
@@ -18,9 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', include('appHarryPotter.urls')), 
+    path('', RedirectView.as_view(url='/appHarryPotter/', permanent=False)),  # Para redirigir
+    path('appHarryPotter/', include('appHarryPotter.urls')),
     path("admin/", admin.site.urls),
 ]
 
--- appHarryPotter/urls.py
+++ appHarryPotter/urls.py
@@ -4,14 +4,14 @@ from . import views
 # Definimos las rutas para que soporten las traducciones
 urlpatterns = [
  path('', views.IndexView.as_view(), name='index'),
- path('appHarryPotter/categorias', views.CategoriasListView.as_view(), name="categorias"),
- path('appHarryPotter/categorias/<int:pk>/criaturas/', views.CriaturasPorCategoriaView.as_view(), name='criaturas_por_categoria'),
- path('appHarryPotter/razas', views.RazasListView.as_view(), name="razas"),
- path('appHarryPotter/razas/<int:pk>/criaturas/', views.CriaturasPorRazaView.as_view(), name='criaturas_por_raza'),
- path('appHarryPotter/criaturas', views.CriaturasListView.as_view(), name="criaturas"),
- path('appHarryPotter/criatura/<int:pk>/', views.CriaturaDetailView.as_view(), name='criatura'),
- path('appHarryPotter/formulario/', views.formularios, name='formulario'),
- path('set-language/<str:lang_code>/', views.set_language, name='set_language'),
- path('appHarryPotter/nueva_raza/', views.nueva_raza, name='nueva_raza'),
+ path('categorias', views.CategoriasListView.as_view(), name="categorias"),
+ path('categorias/<int:pk>/criaturas/', views.CriaturasPorCategoriaView.as_view(), name='criaturas_por_categoria'),
+ path('razas', views.RazasListView.as_view(), name="razas"),
+ path('razas/<int:pk>/criaturas/', views.CriaturasPorRazaView.as_view(), name='criaturas_por_raza'),
+ path('criaturas', views.CriaturasListView.as_view(), name="criaturas"),
+ path('criatura/<int:pk>/', views.CriaturaDetailView.as_view(), name='criatura'),
+ path('formulario/', views.formularios, name='formulario'),
+ path('<str:lang_code>/', views.set_language, name='set_language'),
+ path('nueva_raza/', views.nueva_raza, name='nueva_raza'),
  path('buscar-criaturas/', views.buscar_criaturas, name='buscar_criaturas'),
 ]
